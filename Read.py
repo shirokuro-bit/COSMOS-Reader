af4580532d74fb99fd97b51aa66cf8834f9f4591
shirokuro <65277895+shirokuro-bit@users.noreply.github.com>
2020-09-09 15:15:15 +0900

    完成

--- Read.py
+++ Read.py
@@ -1,29 +1,49 @@
+#!/usr/bin/env python
 # モジュール読み込み
 import datetime
+import time
 
 import pymysql.cursors
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
-# 現在時刻
-dt_now = datetime.datetime.now()
+while 1:
+    zaishitu = 0
 
-print(dt_now)
+    # 現在時刻
+    dt_now = datetime.datetime.now()
 
-# MySQLに接続する
-connection = pymysql.connect(
-    host='192.168.11.7',
-    user='COSMOS',
-    password='PASSWORD',
-    db='room_management',
-    charset='utf8'
-)
+    # 在室の判断
+    reader = SimpleMFRC522()
+    try:
+        id, text = reader.read()
+        if ('true' in text):
+            reader.write("false")
+            zaishitu = 0
+        elif ('false' in text):
+            reader.write("true")
+            zaishitu = 1
+    finally:
+        GPIO.cleanup()
 
-# Insert処理
-with connection.cursor() as cursor:
-    sql = "INSERT INTO zaishitu VALUES (%s, %s, %s)"
-    cursor.execute(sql, (dt_now,  901, 0))
-    # autocommitではないので、明示的にコミットする
-    connection.commit()
+    print("read")
 
-# MySQLから切断する
-connection.close()
+    # MySQLに接続する
+    connection = pymysql.connect(
+        host='localhost',
+        user='COSMOS',
+        password='PASSWORD',
+        db='room_management',
+        charset='utf8'
+    )
 
+    # Insert処理
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO zaishitu VALUES (%s, %s, %s)"
+        cursor.execute(sql, (dt_now,  id, zaishitu))
+        # autocommitではないので、明示的にコミットする
+        connection.commit()
+
+    # MySQLから切断する
+    connection.close()
+    time.sleep(2)
