2f7842eff12b291ce2d29c5ff352e3bd3de23749
shirokuro-bit <shirokuro.kazu@gmail.com>
2020-12-22 03:08:16 +0900

    参考サイトのURLのメモを書き足し

--- Read.py
+++ Read.py
@@ -40,10 +40,16 @@ while 1:
     # Insert処理
     with connection.cursor() as cursor:
         sql = 'INSERT INTO zaishitu VALUES (%s, %s, %s)'
-        cursor.execute(sql, (dt_now,  id, zaishitu))
+        cursor.execute(sql, (dt_now, id, zaishitu))
         # autocommitではないので、明示的にコミットする
         connection.commit()
 
     # MySQLから切断する
     connection.close()
     time.sleep(2)
+
+# https://pimylifeup.com/raspberry-pi-rfid-rc522/
+
+# https://qiita.com/utti0307/items/8abbea38bf78ff4abfa0
+# https://qiita.com/johndoe1022/items/0c704a64a38d876e8bdf
+# https://www.google.com/search?client=ms-android-samsung-ss&sxsrf=ALeKk01EGYCg3H2Q6gayqkUNqX8wdlz4wQ%3A1608573337105&ei=meHgX4ziBdbWhwP71beICA&q=python+mariadb&oq=python+Maria&gs_lcp=ChNtb2JpbGUtZ3dzLXdpei1zZXJwEAEYADICCAAyAggAMgIIADICCAAyAggAMgUIABDLATIFCAAQywEyBQgAEMsBOgQIABBHOgQIIxAnOgoIABCxAxCDARAEOgoIABCxAxCDARBDOgQIABAEOgQIABBDOggIABCxAxCDAToFCAAQsQNQvSNY2XFgyH5oAHABeACAAZQCiAGNDJIBBTAuMS42mAEAoAEByAEEwAEB&sclient=mobile-gws-wiz-serp
