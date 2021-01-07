48452dfa654b395d183b540cf1555bae1f938d1d
shirokuro-bit <shirokuro.kazu@gmail.com>
2021-01-08 03:23:20 +0900

    LED点灯(test)

---
+++ LED.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import time
+
+# import RPi.GPIO as GPIO
+
+# GPIO.setmode(GPIO.BCM)  # GPIO番号で指定
+
+
+class LED:
+    def enter(self):
+        # GPIO.setup(25, GPIO.OUT)
+        #
+        # GPIO.output(25, GPIO.HIGH)
+        # time.sleep(2)
+        #
+        # GPIO.cleanup()
+        print('退室')
+
+    def exit(self):
+        # GPIO.setup(26, GPIO.OUT)
+        #
+        # GPIO.output(26, GPIO.HIGH)
+        # time.sleep(2)
+        #
+        # GPIO.cleanup()
+        print('入室')
+
+    def Sing_Up(self):
+        # GPIO.setup(26, GPIO.OUT)
+        #
+        # GPIO.output(26, GPIO.HIGH)
+        # time.sleep(2)
+        #
+        # GPIO.cleanup()
+        print('新規登録')
